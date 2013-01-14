8db6666081a5a1a7e5833855ffbac685a47c470e
feelx88 <feelx88@googlemail.com>
2013-01-14 07:57:39 +0100

    Added README and COPYING files and inserted GPL headers to all java files.

--- src/de/feelx88/flashlighttoggler/FLTSettings.java
+++ src/de/feelx88/flashlighttoggler/FLTSettings.java
@@ -1,3 +1,22 @@
+/**
+Copyright 2013 Felix Müller.
+
+This file is part of FlashLightToggler.
+
+FlashLightToggler is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FlashLightToggler is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FlashLightToggler.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package de.feelx88.flashlighttoggler;
 
 import java.util.List;
