4e3c4f7d8c0dfafb3a74d543ca48a1b130b88c93
feelx88 <feelx88@googlemail.com>
2013-01-14 07:33:37 +0100

    Changed task-affinity of Settings activity to non-default value to allow launching MainActivity when Settings has been openend before; Added notification in status bar to allow switching off again by clicking on it.

--- src/de/feelx88/flashlighttoggler/FLTSettings.java
+++ src/de/feelx88/flashlighttoggler/FLTSettings.java
@@ -6,7 +6,6 @@ import android.annotation.TargetApi;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
--- src/de/feelx88/flashlighttoggler/FlashLightService.java
+++ src/de/feelx88/flashlighttoggler/FlashLightService.java
@@ -1,18 +1,21 @@
 package de.feelx88.flashlighttoggler;
 
-import android.annotation.TargetApi;
+import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.IBinder;
+import android.widget.Toast;
 
 
 public class FlashLightService extends Service {
 
 	private Camera mCamera;
+	private Notification mNotification;
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -34,35 +37,39 @@ public class FlashLightService extends Service {
         createNotification();
 	}
 	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@SuppressLint("NewApi")
 	private void createNotification()
 	{
 		PendingIntent intent = PendingIntent.getActivity( this, 0,
         		new Intent( this, MainActivity.class ), PendingIntent.FLAG_ONE_SHOT );
 		
-		new Notification.Builder( this )
-	        .setContentTitle( "Flash light activated" )
-	        .setContentText( "Click to disable" )
-	        .setSmallIcon( R.drawable.ic_flashlight_on )
-	        .setOngoing( true )
-	        .setPriority( Notification.PRIORITY_HIGH )
-	        .setContentIntent( intent )
-	        .build();
-	}
-	
-	private void createNotification()
-	{
-		PendingIntent intent = PendingIntent.getActivity( this, 0,
-        		new Intent( this, MainActivity.class ), PendingIntent.FLAG_ONE_SHOT );
+		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+		{
 		
-		new Notification.Builder( this )
-	        .setContentTitle( "Flash light activated" )
-	        .setContentText( "Click to disable" )
-	        .setSmallIcon( R.drawable.ic_flashlight_on )
-	        .setOngoing( true )
-	        .setPriority( Notification.PRIORITY_HIGH )
-	        .setContentIntent( intent )
-	        .build();
+			mNotification = new Notification.Builder( this )
+		        .setContentTitle( "Flash light activated" )
+		        .setContentText( "Click to disable" )
+		        .setSmallIcon( R.drawable.ic_flashlight_on )
+		        .setOngoing( true )
+		        .setPriority( Notification.PRIORITY_HIGH )
+		        .setContentIntent( intent )
+		        .build();
+		}
+		else if(  Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB )
+		{
+			mNotification = new Notification.Builder( this )
+		        .setContentTitle( "Flash light activated" )
+		        .setContentText( "Click to disable" )
+		        .setSmallIcon( R.drawable.ic_flashlight_on )
+		        .setOngoing( true )
+		        .setContentIntent( intent )
+		        .build();
+		}
+		else
+			Toast.makeText( this, "Started flash light", Toast.LENGTH_SHORT ).show();
+		
+		if( mNotification != null )
+			( (NotificationManager)getSystemService( NOTIFICATION_SERVICE ) ).notify( 0, mNotification );
 	}
 	
 	@Override
@@ -70,6 +77,7 @@ public class FlashLightService extends Service {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		mCamera.release();
+		( (NotificationManager)getSystemService( NOTIFICATION_SERVICE ) ).cancel( 0 );
 	}
 
 }
