bedaed6fc9ffb0f901c20a4d709e273c186bcffe
feelx88 <feelx88@googlemail.com>
2013-02-04 11:29:38 +0100

    Removed menu and layot xml as they are not needed; removed setContentView call as the Activity is immediatly closed.

--- src/de/feelx88/flashlighttoggler/MainActivity.java
+++ src/de/feelx88/flashlighttoggler/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         
         mPolicyManager = (DevicePolicyManager)getSystemService( DEVICE_POLICY_SERVICE );
         mAdminReceiver = new ComponentName( this, FLTDeviceAdminReceiver.class );
@@ -84,13 +83,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
     	super.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
     }
     
 }
